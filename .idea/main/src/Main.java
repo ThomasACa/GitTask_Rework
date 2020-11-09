c70fc253f82fb61b88bf96ed60ca8fd41b0b7628
ArtiomC <artiom.tomas@gmail.com>
2020-11-09 17:07:45 +0200

    Task 7 commit 1 Branch1

--- .idea/main/src/Main.java
+++ .idea/main/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(myMethod(3));
+        myMethodTwo();
     }
+
+    static int myMethod(int x) {
+        return 5 + x;
+    }
+    static void myMethodTwo() {
+        System.out.println("Hello there!");
+    }
+
+
 }
