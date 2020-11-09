9a0e34e7af9b0d19eba9018764d27360a2d9e7bf
ArtiomC <artiom.tomas@gmail.com>
2020-11-09 17:17:01 +0200

    Task 7 commit 2 Branch2

--- .idea/main/src/Main.java
+++ .idea/main/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Task 6 make changes to your existing class");
+        myMethodThree();
+        myMethodFour(3);
     }
+
+    static void myMethodThree() {
+        System.out.println("My Method Three");
+    }
+    static int myMethodFour(int x) {
+        return 5 + x;
+    }
+
+
 }
